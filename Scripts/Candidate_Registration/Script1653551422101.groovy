100f34e37c553c3fded7827d08a7d234804d2743
atish <atish.sharma59@gmail.com>
2022-08-03 13:13:44 +0530

    Commit-3August

--- Scripts/Candidate_Registration/Script1653551422101.groovy
+++ Scripts/Candidate_Registration/Script1653551422101.groovy
@@ -3,10 +3,6 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import javax.swing.ListSelectionModel as ListSelectionModel
-import javax.swing.text.html.CSS
-import javax.swing.text.html.FormView
-
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
@@ -16,21 +12,29 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import groovy.transform.ThreadInterrupt as ThreadInterrupt
+import groovy.ui.SystemOutputInterceptor as SystemOutputInterceptor
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.By as By
 import org.openqa.selenium.Keys as Keys
 import org.openqa.selenium.WebDriver as WebDriver
 import org.openqa.selenium.WebElement as WebElement
-import org.openqa.selenium.interactions.Actions as Actions
-import org.openqa.selenium.remote.DriverCommand as DriverCommand
-import org.openqa.selenium.remote.server.DriverFactory as DriverFactory
-import org.openqa.selenium.By.ByCssSelector as ByCssSelector
-import org.apache.commons.lang.RandomStringUtils
+import org.openqa.selenium.interactions.Actions
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import org.testng.Assert as Assert
+import org.openqa.selenium.JavascriptExecutor as JavascriptExecutor
+import org.apache.commons.lang.RandomStringUtils as RandomStringUtils
 
-import java.awt.List
-import java.io.BufferedReader as BufferedReader
-import java.text.DateFormat as DateFormat
-import java.text.SimpleDateFormat as SimpleDateFormat
+WebUI.openBrowser('')
 
+WebUI.maximizeWindow()
+WebDriver driver = DriverFactory.getWebDriver()
+WebUI.navigateToUrl('https://testaccount.motzie-staging.mobile-recruit.com/register?q=EQOuCSHP')
+WebDriverWait wait = new WebDriverWait(driver, 10);
+WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath('//*[@id="contentMainDiv"]/candidate-register/section/div/div[1]/div/div[2]/div/form/div[6]/div/label/text()')));
+element.click();
+//driver.findElement(By.cssSelector('form[role="login"] > .boost-parameters')).click()
